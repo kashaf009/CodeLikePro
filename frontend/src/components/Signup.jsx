2a8819065f02ab3963c060f91b3770592a63e611
kashaf khan <mdkashaf9931@gmail.com>
2026-05-15 18:13:50 +0530

    feat:made signup inputs and buttoms

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -6,19 +6,19 @@ import { FaCode } from "react-icons/fa";
 
 const Signup = () => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="md:grid md:grid-cols-2 ">
       {/* left side  */}
-      <div className="pl-20 pt-30 w-full min-h-screen  bg-[radial-gradient(circle_at_top_left,rgba(76,215,246,0.12),transparent_35%),radial-gradient(circle_at_bottom_right,rgba(109,40,255,0.15),transparent_40%),linear-gradient(180deg,#102A43_0%,#07192E_100%)]">
-        <h1 className="font-['inter'] flex items-center gap-2 text-4xl text-[#A8B3C7] tracking-tight font-extrabold mb-10">
+      <div className=" hidden md:block md:pl-25 md:pt-30 md:w-full min-h-screen  md:bg-[radial-gradient(circle_at_top_left,rgba(76,215,246,0.12),transparent_35%),radial-gradient(circle_at_bottom_right,rgba(109,40,255,0.15),transparent_40%),linear-gradient(180deg,#102A43_0%,#07192E_100%)]">
+        <h1 className="font-['inter'] flex items-center gap-2 text-4xl text-[#A8B3C7] tracking-tight font-black mb-10">
           CodeLikePro
           <span className="">
             <FaCode />
           </span>
         </h1>
-        <p className="font-['inter'] mb-3 text-5xl font-extrabold tracking-tight text-[#dfe9f6]">
+        <p className="font-['inter'] mb-3 text-5xl font-black tracking-tight text-[#dfe9f6]">
           Engineer your
         </p>
-        <p className="font-['inter'] text-5xl font-extrabold tracking-tight text-[#5BE7FF]">
+        <p className="font-['inter'] text-5xl font-black tracking-tight text-[#5BE7FF]">
           career trajectory.
         </p>
 
@@ -33,40 +33,100 @@ const Signup = () => {
         </p>
 
         <div className="w-[55%] mt-15 flex justify-between">
-          <div className="font-['inter'] text-3xl text-[#dfe9f6] font-bold ">
+          <div className="font-['inter'] text-3xl text-[#dfe9f6] font-black ">
             {" "}
             5k+
             <br />
-            <p className="text-[12px] mt-1 font-extrabold text-[#9ba6bb]">
+            <p className="text-[12px] mt-1 tracking-tight font-extrabold text-[#9ba6bb]">
               ACTIVE STUDENTS
             </p>
           </div>
-          <div className="font-['inter'] text-3xl text-[#dfe9f6] font-bold ">
+          <div className="font-['inter'] text-3xl text-[#dfe9f6] font-black ">
             {" "}
             20+
             <br />
-            <p className="text-[12px] mt-1 font-extrabold text-[#9ba6bb]">
+            <p className="text-[12px] mt-1 tracking-tight font-extrabold text-[#9ba6bb]">
               PRO BOOTCAMP
             </p>
           </div>
         </div>
         {/* testimonial */}
-          <div className="bg-[#132339] rounded-md border border-gray-700 mt-10 h-40 w-[60%]">
-            <div className="pl-5 pt-4 pr-5">
-              <p className="text-[#eaf0fa]">Marcus Chen</p>
-              <p className="text-xs mb-3 text-[#BFC7D5]">L6 Engineer @ CloudStack</p>
-                <p className="text-[#BFC7D5]">"The curriculum didn't just teach me syntax; it taught me how to think like a staff engineer. The Code Sandbox is a game changer."</p>
-            </div>
-
-
+        <div className="bg-[#132339] rounded-md mb-20 border border-gray-700 mt-10 h-40 w-[60%]">
+          <div className="pl-5 pt-4 pr-5">
+            <p className="text-[#eaf0fa] font-bold">Marcus Chen</p>
+            <p className="text-xs mb-3 text-[#BFC7D5]">
+              L6 Engineer @ CloudStack
+            </p>
+            <p className="text-[#BFC7D5]">
+              "The curriculum didn't just teach me syntax; it taught me how to
+              think like a staff engineer. The Code Sandbox is a game changer."
+            </p>
           </div>
-
-
+        </div>
       </div>
 
       {/* signup side */}
-      <div className="p-20 pt-25 bg-[linear-gradient(180deg,#051424_0%,#020817_100%)]">
-        <h1 className="text-white">Start Your Journey.</h1>
+      <div className="md:px-25 px-10 pb-20  pt-15 h-screen bg-[linear-gradient(180deg,#051424_0%,#020817_100%)]">
+        <section className="w-[95%] md:w-[90%]">
+          <h1 className="font-['inter'] text-[25px] md:text-[50px] tracking-tighter  text-[#dfe9f6]  font-black ">
+            Start Your Journey.
+          </h1>
+          <p className="font-['inter'] text-[12px]  md:text-xl text-[#A8B3C7] tracking-tight font-extrabold ">
+            Configure your development profile to begin.
+          </p>
+
+          <div className="mt-10  ">
+            <label
+              className="text-[#cdd3de]  text-xs  font-black tracking-tighter font-['inter']"
+              htmlFor="name"
+            >
+              FULL NAME
+            </label>
+            <input
+              className="w-full mt-2 border rounded-md outline-[#31d8f5] border-gray-600 text-[#dfe9f6] py-2 px-4"
+              id="name"
+              type="text"
+            />
+          </div>
+          <div className="mt-7  ">
+            <label
+              className="text-[#cdd3de]  text-xs  font-black tracking-tighter font-['inter']"
+              htmlFor="email"
+            >
+              EMAIL
+            </label>
+            <input
+              className="w-full mt-2 border rounded-md outline-[#31d8f5] border-gray-600 text-[#dfe9f6] py-2 px-4"
+              id="email"
+              type="email"
+            />
+          </div>
+          <div className="mt-7  ">
+            <label
+              className="text-[#cdd3de]  text-xs  font-black tracking-tighter font-['inter']"
+              htmlFor="password"
+            >
+              PASSWORD
+            </label>
+            <input
+              className="w-full mt-2 border rounded-md outline-[#31d8f5] border-gray-600 text-[#dfe9f6] py-2 px-4"
+              id="password"
+              type="password"
+            />
+          </div>
+          <div className="mt-7  ">
+            <label
+              className="text-[#cdd3de]  text-xs  font-black tracking-tighter font-['inter']"
+              htmlFor="password"
+            >
+              ROLE
+            </label>
+            <div className="flex items-center justify-center gap-5 mt-3">
+              <span className="text-[#cdd3de] border py-1 px-3 md:py-2 md:px-6 border-gray-600 hover:border-[#31d8f5] rounded-md">STUDENT</span>
+              <span className="text-[#cdd3de]  py-1 px-3 md:py-2 md:px-6 border border-gray-600 hover:border-[#31d8f5] rounded-md">EDUCATOR</span>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
