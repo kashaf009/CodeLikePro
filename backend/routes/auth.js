183624c2bd3fec2f5fd4c7139ec2f6ff94986c67
kashaf khan <mdkashaf9931@gmail.com>
2026-06-02 17:58:42 +0530

    feat : made googlesignin api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -65,7 +65,6 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       password: hashPass,
       role,
-
     });
 
     const token = await getToken(User._id);
@@ -83,7 +82,7 @@ authRouter.post("/signup", async (req, res) => {
       name: User.name,
       emailId: User.emailId,
       role: User.role,
-      photoUrl:User.photoUrl
+      photoUrl: User.photoUrl,
     };
 
     return res
@@ -93,7 +92,6 @@ authRouter.post("/signup", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
 
 // login
 authRouter.post("/login", async (req, res) => {
@@ -108,13 +106,13 @@ authRouter.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Enter valid EmailId" });
     }
 
-    const verifiedUser =await user.findOne({emailId});
+    const verifiedUser = await user.findOne({ emailId });
 
     if (!verifiedUser) {
       return res.status(404).json({ message: "Invalid Credentials" });
     }
 
-    const VerifyPass =await bcrypt.compare(password, verifiedUser.password);
+    const VerifyPass = await bcrypt.compare(password, verifiedUser.password);
 
     if (!VerifyPass) {
       return res.status(404).json({ message: "Incorrect Password" });
@@ -123,11 +121,11 @@ authRouter.post("/login", async (req, res) => {
     const token = getToken(verifiedUser._id);
 
     const safeinfo = {
-      id:verifiedUser._id,
-      name:verifiedUser.name,
-      emailId:verifiedUser.emailId,
-      role:verifiedUser.role,
-      photoUrl:verifiedUser.photoUrl
+      id: verifiedUser._id,
+      name: verifiedUser.name,
+      emailId: verifiedUser.emailId,
+      role: verifiedUser.role,
+      photoUrl: verifiedUser.photoUrl,
     };
 
     res.cookie("token", token, {
@@ -137,13 +135,13 @@ authRouter.post("/login", async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.status(200).json({ message: "login Successfull", user:safeinfo });
+    return res
+      .status(200)
+      .json({ message: "login Successfull", user: safeinfo });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 });
-
-
 
 // logout
 authRouter.get("/logout", async (req, res) => {
@@ -156,6 +154,51 @@ authRouter.get("/logout", async (req, res) => {
     return res.status(200).json({ message: "logout successfull" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
+  }
+});
+
+authRouter.post("/googleSignin", async (req, res) => {
+  try {
+    const { name, emailId, password, role } = req.body;
+
+    const checkEmail = user.findOne({ emailId });
+
+    if (!checkEmail) {
+      const newUser = new user({
+        name,
+        emailId,
+        password,
+        role,
+      });
+
+      await newUser.save();
+
+    }
+
+    const token = await getToken(newUser._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    const safeinfo = {
+      id: checkEmail._id || newUser._id,
+      name: checkEmail.name || newUser.name ,
+      emailId: checkEmail.emailId || newUser.emailId,
+      role: checkEmail.role || newUser.role,
+      photoUrl: checkEmail.photoUrl || newUser.photoUrl,
+    };
+
+
+    res.status(200).json({message:"google signin successfull",
+      user: safeinfo
+    })
+
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
