3a7aaeed23e98f4a3a93d096c0a1e3c4440774ca
kashaf khan <mdkashaf9931@gmail.com>
2026-06-16 17:03:08 +0530

    feat : made create course section

--- frontend/src/components/dashboard/CreateCourse.jsx
+++ frontend/src/components/dashboard/CreateCourse.jsx
@@ -1,19 +1,30 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { BASE_URL } from "../../utils/constants.js";
 import axios from "axios";
+import { ImSpinner2 } from "react-icons/im";
+import { IoCheckmarkDoneCircle, IoChevronBack } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+
 
 const CreateCourse = () => {
   const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const [loading, setloading] = useState(false);
+  const [Error, setError] = useState(null);
+  const [state, setstate] = useState(1);
 
   const titleRef = useRef(null);
   const categoryRef = useRef(null);
   const userId = user.id;
 
- console.log("kashaf");
- 
-  const handleCreateCourse =async () => {
+  const handleCreateCourse = async () => {
+    setError(null);
     try {
+      if (!titleRef || !categoryRef) {
+        setError("all fields required");
+      }
+      setloading(true);
       const res = await axios.post(
         BASE_URL + "/createCourse",
         {
@@ -26,68 +37,101 @@ const CreateCourse = () => {
         },
       );
 
-      console.log(res.data);
+      if (res.status === 200) {
+        setstate(2);
+      }
 
-
+      console.log("course created successfully");
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message || "something went wrong");
+    } finally {
+      setloading(false);
     }
   };
 
-  return ( 
-    <div className="p-4 px-30  pt-15 bg-slate-950 min-h-screen text-white">
-      <h1 className="text-3xl  text-center font-bold mb-1">Create Course</h1>
-
-      <div className="pt-15 px-10 pb-15 rounded-2xl flex mx-auto w-[50%] bg-slate-800 flex-col  mt-5">
-        {/* label for course title and category */}
-        <label htmlFor="course-title" className="font-medium mb-2">
-          Course Title
-        </label>
-
-        <form onSubmit={(e) => e.preventDefault()} className="flex flex-col">
-          <input
-            type="text"
-            ref={titleRef}
-            id="course-title"
-            placeholder="Course Title"
-            className="border mb-5 border-gray-700 px-4 py-2 rounded-md bg-slate-800 text-white"
-          />
-
-          <label htmlFor="category" className="font-medium mb-2">
-            Category
+  return (
+    <div className="p-4 px-30 relative  pt-30 bg-slate-950 min-h-screen text-white">
+      <IoChevronBack onClick={()=>navigate("/dashboard/courses")} className="absolute hover:text-cyan-500 cursor-pointer top-37 w-8 h-8 left-60" />
+      {state == 1 && (
+        <div className="pt-10 px-10 pb-15 rounded-2xl flex mx-auto w-[50%] bg-slate-800 flex-col  mt-5">
+          {/* label for course title and category */}
+          <h1 className="text-3xl font-['ibm_plex_mono'] mb-10  text-center font-bold ">
+            Create Course
+          </h1>
+          <label htmlFor="course-title" className="font-medium  mb-2">
+            Course Title
           </label>
 
-          {/* category ["web development","mobile development","data science","artificial intelligence","cloud computing","cyber security","agentic ai","other","machine learning","data analysis"] */}
-          <select
-            ref={categoryRef}
-            id="category"
-            className="border border-gray-700 px-4 mb-8 py-2 rounded-md bg-slate-800 text-white"
-          >
-            <option value="">Select Category</option>
-            <option value="web-development">Web Development</option>
-            <option value="mobile-development">Mobile Development</option>
-            <option value="data-science">Data Science</option>
-            <option value="artificial-intelligence">
-              Artificial Intelligence
-            </option>
-            <option value="cloud-computing">Cloud Computing</option>
-            <option value="cyber-security">Cyber Security</option>
-            <option value="agentic-ai">Agentic AI</option>
+          <form onSubmit={(e) => e.preventDefault()} className="flex flex-col">
+            <input
+              type="text"
+              ref={titleRef}
+              onChange={() => setError(null)}
+              id="course-title"
+              placeholder="Course Title"
+              className="border mb-5 border-gray-700 px-4 py-2 rounded-md bg-slate-800 text-white"
+            />
 
-            <option value="machine-learning">Machine Learning</option>
-            <option value="data-analysis">Data Analysis</option>
-            <option value="other">Other</option>
-          </select>
+            <label htmlFor="category" className="font-medium mb-2">
+              Category
+            </label>
 
-          <button
-            onClick={handleCreateCourse}
-            type="submit"
-            className="bg-blue-900 hover:bg-blue-700 px-4 py-2 rounded-md text-white"
+            {/* category ["web development","mobile development","data science","artificial intelligence","cloud computing","cyber security","agentic ai","other","machine learning","data analysis"] */}
+            <select
+              ref={categoryRef}
+              onChange={() => setError(null)}
+              id="category"
+              className={`border border-gray-700 px-4 py-2 rounded-md bg-slate-800 text-white ${Error ? "mb-5" : "mb-8"}`}
+            >
+              <option value="">Select Category</option>
+              <option value="web development">Web Development</option>
+              <option value="mobile development">Mobile Development</option>
+              <option value="data science">Data Science</option>
+              <option value="artificial intelligence">
+                Artificial Intelligence
+              </option>
+              <option value="cloud computing">Cloud Computing</option>
+              <option value="cyber security">Cyber Security</option>
+              <option value="agentic ai">Agentic AI</option>
+
+              <option value="machine learning">Machine Learning</option>
+              <option value="data analysis">Data Analysis</option>
+              <option value="other">Other</option>
+            </select>
+
+            {Error && <p className="text-red-500 text-md mb-2">{Error}</p>}
+
+            <button
+              onClick={handleCreateCourse}
+              type="submit"
+              className="bg-blue-900 cursor-pointer font-['ibm_plex_mono'] hover:bg-blue-700 px-4 py-2 rounded-md text-white"
+            >
+              {loading ? (
+                <>
+                  <ImSpinner2 className="animate-spin mx-auto text-md  justify-center " />
+                </>
+              ) : (
+                "Create"
+              )}
+            </button>
+          </form>
+        </div>
+      )}
+
+      {state == 2 && (
+        <div className="pt-10 px-10 pb-10 rounded-2xl justify-center items-center flex mx-auto w-[50%] bg-slate-800 flex-col  mt-5">
+          <h1 className="text-2xl mb-5 text-center font-['ibm_plex_mono'] tracking-tight font-bold">
+            Course created successfully
+          </h1>
+          <IoCheckmarkDoneCircle className="w-40 mb-3 fill-green-500 h-40 " />
+          <div
+            onClick={() => navigate("/dashboard/courses")}
+            className="bg-blue-900 hover:bg-blue-700 cursor-pointer rounded-md px-3 py-1"
           >
-            Create Course
-          </button>
-        </form>
-      </div>
+            <p className="font-['ibm_plex_mono']">Back to creator page</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/components/dashboard/CreatorCourses.jsx
+++ frontend/src/components/dashboard/CreatorCourses.jsx
@@ -1,64 +1,80 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../../utils/constants";
+import { FiEdit } from "react-icons/fi";
 
 const CreatorCourses = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [courses, setcourses] = useState([]);
 
-    const getCourses = async () => {
-        // api call to get courses for the creator
-        const res = await axios.get(BASE_URL + "/MyCourses", {
-            withCredentials: true,
-        });
-        console.log(res?.data?.data);  
-    }
+  const getCourses = async () => {
+    // api call to get courses for the creator
+    const res = await axios.get(BASE_URL + "/MyCourses", {
+      withCredentials: true,
+    });
+    console.log(res?.data?.data);
+    setcourses(res?.data?.data || []);
+  };
 
-
-    useEffect(() => {
-        // fetch courses for the creator
-        getCourses();
-    }, [])  
-
-
-
-
-
+  useEffect(() => {
+    // fetch courses for the creator
+    getCourses();
+  }, []);
 
   return (
     <div className="p-4 px-30 pt-15 bg-slate-900 min-h-screen text-white">
+      <div
+        onClick={() => navigate("/dashboard/create-course")}
+        className="absolute text-white text-xl flex items-center cursor-pointer hover:bg-blue-700 gap-2 bg-blue-900 px-3 rounded-md py-1  top-16 right-31"
+      >
+        <div>
+          <AiOutlinePlus className="text-md" />
+        </div>
+        <p> Create course</p>
+      </div>
 
-        <div onClick={()=> navigate('/dashboard/create-course')} className="absolute text-white text-xl flex items-center cursor-pointer hover:bg-blue-700 gap-2 bg-blue-900 px-3 rounded-md py-1  top-16 right-31">
-                <div><AiOutlinePlus className="text-md" /></div>
-            <p> Create course</p>
-            </div>
-
-        <h1 className="text-3xl font-bold mb-1">Your Courses</h1>
-        <table className="w-full  border border-gray-700 mt-5">
-            <thead>
-                <tr>
-                    <th className="border border-gray-700 px-4 py-2">thumbnail</th>
-                    <th className="border border-gray-700 px-4 py-2">Course Title</th>
-                    <th className="border border-gray-700 px-4 py-2">Status</th>
-                    <th className="border border-gray-700 px-4 py-2">Action</th>
-                </tr>
-            </thead>
-           <tbody>
-                {<tr>
-                    <td className="border border-gray-700 w-1/4 px-4 py-2">
-                        <img src="course-thumbnail.jpg" alt="Course Thumbnail" className="w-full h-full object-cover" />
-                    </td>
-                    <td className="border border-gray-700 px-4 py-2">Course 1</td>
-                    <td className="border border-gray-700 px-4 py-2">0</td>
-                    <td  className="border border-gray-700 px-4 py-2">edit</td>
-                </tr>}
-                {/* Add more courses as needed */}
-            </tbody>
-        </table>
-        
+      <h1 className="text-3xl font-bold mb-1">Your Courses</h1>
+      <table className="w-full   border border-gray-700 mt-10">
+        <thead>
+          <tr>
+            <th className="border border-gray-700 px-4 py-2">thumbnail</th>
+            <th className="border border-gray-700 px-4 py-2">Course Title</th>
+            <th className="border border-gray-700 px-4 py-2">Status</th>
+            <th className="border border-gray-700 px-4 py-2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr key={course._id}>
+              <td className="border border-gray-700 w-1/4 px-4 py-2">
+                <img
+                  src={course?.thumbnail}
+                  alt="Course Thumbnail"
+                  className="w-full px-3 py-2 h-full object-cover"
+                />
+              </td>
+              <td className="border  border-gray-700 pl-10 pr-5  text-2xl font-bold py-2">
+                {course.title}
+              </td>
+              <td
+                className={`border border-gray-700 z-10 text-center text-black  px-4 py-2 ${course.ispublished == true ? "text-green-500" : "text-red-400"}`}
+              >
+            {course.ispublished == true ? "published" : "draft"}
+              </td>
+              <td className="border  border-gray-700 px-4 py-2">
+                <div className="w-full flex">
+                  <FiEdit className="mx-auto" />
+                </div>
+              </td>
+            </tr>
+          ))}
+          {/* Add more courses as needed */}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default CreatorCourses
+export default CreatorCourses;
