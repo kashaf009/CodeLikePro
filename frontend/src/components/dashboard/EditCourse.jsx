e2d9774c26ba759f1b9e3b775a4a6fabbd45cb1f
kashaf khan <mdkashaf9931@gmail.com>
2026-07-05 22:41:23 +0530

    feat : improved edit course section

--- frontend/src/components/dashboard/EditCourse.jsx
+++ frontend/src/components/dashboard/EditCourse.jsx
@@ -20,6 +20,8 @@ const EditCourse = () => {
   const [ispublished, setIsPublished] = useState(false);
   const [thumbnail, setThumbnail] = useState(null);
   const [NewThumbnail, setNewThumbnail] = useState(null);
+  const [DeleteSuccessfull, setDeleteSuccessfull] = useState(false);
+  const [updateSuccessful, setUpdateSuccessful] = useState(false)
 
   const handlePhotoChange = (e) => {
     const file = e.target.files[0];
@@ -63,7 +65,7 @@ const EditCourse = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setloading(true)
+    setloading(true);
 
     try {
       const formData = new FormData();
@@ -87,32 +89,31 @@ const EditCourse = () => {
         },
       );
 
-      alert(res.data.message);
+      setUpdateSuccessful(true)
     } catch (error) {
       console.log("FULL ERROR:", error);
 
       console.log("SERVER RESPONSE:", error.response?.data);
 
       alert(error?.response?.data?.message);
-    }
-    finally{
-      setloading(false)
+    } finally {
+      setloading(false);
     }
   };
 
   const HandleDeleteCourse = async () => {
-    setloading(true)
+    setloading(true);
     try {
       const res = await axios.delete(BASE_URL + `/deleteCourse/${courseId}`, {
         withCredentials: true,
       });
+      setDeleteSuccessfull(true);
 
       console.log(res?.data);
     } catch (error) {
       alert(error?.response?.data?.message);
-    }
-    finally{
-      setloading(false)
+    } finally {
+      setloading(false);
     }
   };
 
@@ -122,7 +123,13 @@ const EditCourse = () => {
 
   return (
     CourseInfo && (
-      <div className="max-w-5xl  mx-auto mb-20 mt-20 bg-slate-800 rounded-2xl shadow-xl overflow-hidden">
+      <div
+        className={
+          DeleteSuccessfull === false && updateSuccessful === false
+            ? `max-w-5xl  mx-auto mb-20 mt-20 bg-slate-800 rounded-2xl shadow-xl overflow-hidden`
+            : ` bg-slate-950`
+        }
+      >
         {/* Header */}
         <div
           onClick={() => navigate("/dashboard/courses")}
@@ -132,149 +139,226 @@ const EditCourse = () => {
           <MdArrowBackIos />
         </div>
 
-        <div className="px-8 py-6 border-b border-slate-700">
-          <h1 className="text-3xl pl-3 font-bold text-white">Edit Course</h1>
-          <p className="text-slate-400 mt-1 pl-3">
-            Update your course details and thumbnail
-          </p>
+        {DeleteSuccessfull == false && updateSuccessful=== false &&(
+          <>
+            (
+            <div className="px-8 py-6 border-b border-slate-700">
+              <h1 className="text-3xl pl-3 font-bold text-white">
+                Edit Course
+              </h1>
+              <p className="text-slate-400 mt-1 pl-3">
+                Update your course details and thumbnail
+              </p>
 
-          <div
-            onClick={HandleDeleteCourse}
-            className="absolute right-65 top-30 bg-red-400 w-25 items-center gap-1 flex px-3 hover:bg-red-500 cursor-pointer transition-all justify-center rounded-md py-1"
-          >
-           
-            <MdOutlineDeleteForever className={loading ? `hidden` :`w-5 inline h-5`} />{" "}
-            <p className="text-md  font-medium">{loading ?<ImSpinner2 className="animate-spin   py-1  text-2xl "/>:"Delete"}</p>
-          </div>
-        </div>
+              <div
+                onClick={HandleDeleteCourse}
+                className="absolute right-65 top-30 bg-red-400 w-25 items-center gap-1 flex px-3 hover:bg-red-500 cursor-pointer transition-all justify-center rounded-md py-1"
+              >
+                <MdOutlineDeleteForever
+                  className={loading ? `hidden` : `w-5 inline h-5`}
+                />{" "}
+                <p className="text-md  font-medium">
+                  {loading ? (
+                    <ImSpinner2 className="animate-spin   py-1  text-2xl " />
+                  ) : (
+                    "Delete"
+                  )}
+                </p>
+              </div>
+            </div>
+            <div className="p-8 grid lg:grid-cols-2 gap-10">
+              {/* Left Side - Thumbnail */}
+              <div>
+                <label className="block text-white font-medium mb-3">
+                  Course Thumbnail
+                </label>
 
-        <div className="p-8 grid lg:grid-cols-2 gap-10">
-          {/* Left Side - Thumbnail */}
-          <div>
-            <label className="block text-white font-medium mb-3">
-              Course Thumbnail
-            </label>
+                <label className="relative block cursor-pointer group">
+                  <img
+                    src={thumbnail}
+                    alt="thumbnail"
+                    className="w-full h-72 object-cover rounded-xl border border-slate-700"
+                  />
 
-            <label className="relative block cursor-pointer group">
-              <img
-                src={thumbnail}
-                alt="thumbnail"
-                className="w-full h-72 object-cover rounded-xl border border-slate-700"
-              />
+                  <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-xl flex items-center justify-center">
+                    <span className="bg-white text-black px-5 py-2 rounded-lg font-semibold">
+                      Change Thumbnail
+                    </span>
+                  </div>
 
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-xl flex items-center justify-center">
-                <span className="bg-white text-black px-5 py-2 rounded-lg font-semibold">
-                  Change Thumbnail
-                </span>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={handlePhotoChange}
+                  />
+                </label>
+
+                <p className="text-slate-400 text-sm mt-2">
+                  Click the image to upload a new thumbnail.
+                </p>
+
+                <div
+                  onClick={() =>
+                    navigate(
+                      `/dashboard/create-course/edit/createLecture/${courseId}`,
+                    )
+                  }
+                  className="w-full py-1 mt-8 rounded-md hover:bg-blue-500 justify-center cursor-pointer flex items-center gap-1  bg-blue-400"
+                >
+                  <IoIosAdd className="w-5 h-5 text-bold" />
+                  <p className="text-center font-medium">Add Lecture</p>
+                </div>
               </div>
 
-              <input
-                type="file"
-                accept="image/*"
-                className="hidden"
-                onChange={handlePhotoChange}
-              />
-            </label>
+              {/* Right Side - Form */}
+              <div className="space-y-5">
+                <div>
+                  <label className="text-white block mb-2">Course Title</label>
 
-            <p className="text-slate-400 text-sm mt-2">
-              Click the image to upload a new thumbnail.
-            </p>
+                  <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white focus:outline-none focus:border-blue-500"
+                  />
+                </div>
 
-            <div
-              onClick={() =>
-                navigate(
-                  `/dashboard/create-course/edit/createLecture/${courseId}`,
-                )
-              }
-              className="w-full py-1 mt-8 rounded-md hover:bg-blue-500 justify-center cursor-pointer flex items-center gap-1  bg-blue-400"
-            >
-              <IoIosAdd className="w-5 h-5 text-bold" />
-              <p className="text-center font-medium">Add Lecture</p>
+                <div>
+                  <label className="text-white block mb-2">Category</label>
+
+                  <input
+                    type="text"
+                    value={category}
+                    onChange={(e) =>
+                      setCategory(e.target.value.trim().toLowerCase())
+                    }
+                    className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white"
+                  />
+                </div>
+
+                <div>
+                  <label className="text-white block mb-2">Level</label>
+
+                  <select
+                    value={level}
+                    onChange={(e) => setLevel(e.target.value)}
+                    className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white"
+                  >
+                    <option>Beginner</option>
+                    <option>Intermediate</option>
+                    <option>Advanced</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label className="text-white block mb-2">Price (₹)</label>
+
+                  <input
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white"
+                  />
+                </div>
+
+                <div>
+                  <label className="text-white block mb-2">Description</label>
+
+                  <textarea
+                    rows="5"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white resize-none"
+                  />
+                </div>
+
+                <div className="flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    checked={ispublished}
+                    onChange={(e) => setIsPublished(e.target.checked)}
+                    className="w-5 h-5"
+                  />
+
+                  <span className="text-white">Publish Course</span>
+                </div>
+
+                <button
+                  type="submit"
+                  onClick={handleSubmit}
+                  className="w-full bg-blue-600 hover:bg-blue-700 py-3 rounded-lg font-semibold text-white transition-all"
+                >
+                  {loading ? <ImSpinner2 className="text-xl animate-spin  mx-auto  "/> :"Save Changes"}
+                </button>
+              </div>
             </div>
-          </div>
+            )
+          </>
+        )}
 
-          {/* Right Side - Form */}
-          <div className="space-y-5">
-            <div>
-              <label className="text-white block mb-2">Course Title</label>
+        {DeleteSuccessfull && (
+          <div className="flex items-center h-screen justify-center min-h-[500px]">
+            <div className="bg-slate-800 shadow-xl rounded-2xl p-8 w-[420px] text-center border border-slate-700">
+              <div className="flex justify-center mb-4">
+                <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center">
+                  <span className="text-4xl text-white">✓</span>
+                </div>
+              </div>
 
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white focus:outline-none focus:border-blue-500"
-              />
-            </div>
+              <h2 className="text-2xl font-bold text-white mb-2">
+                Course Deleted Successfully
+              </h2>
 
-            <div>
-              <label className="text-white block mb-2">Category</label>
+              <p className="text-slate-400 mb-6">
+                Your course has been permanently deleted.
+              </p>
 
-              <input
-                type="text"
-                value={category}
-                onChange={(e) =>
-                  setCategory(e.target.value.trim().toLowerCase())
-                }
-                className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white"
-              />
-            </div>
-
-            <div>
-              <label className="text-white block mb-2">Level</label>
-
-              <select
-                value={level}
-                onChange={(e) => setLevel(e.target.value)}
-                className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white"
+              <button
+                onClick={() => navigate("/dashboard/courses")}
+                className="w-full bg-blue-600 hover:bg-blue-700  transition-all py-3 rounded-lg text-white font-semibold"
               >
-                <option>Beginner</option>
-                <option>Intermediate</option>
-                <option>Advanced</option>
-              </select>
+                Back to Courses
+              </button>
             </div>
-
-            <div>
-              <label className="text-white block mb-2">Price (₹)</label>
-
-              <input
-                type="number"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-                className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white"
-              />
-            </div>
-
-            <div>
-              <label className="text-white block mb-2">Description</label>
-
-              <textarea
-                rows="5"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="w-full bg-slate-700 border border-slate-600 rounded-lg px-4 py-3 text-white resize-none"
-              />
-            </div>
-
-            <div className="flex items-center gap-3">
-              <input
-                type="checkbox"
-                checked={ispublished}
-                onChange={(e) => setIsPublished(e.target.checked)}
-                className="w-5 h-5"
-              />
-
-              <span className="text-white">Publish Course</span>
-            </div>
-
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="w-full bg-blue-600 hover:bg-blue-700 py-3 rounded-lg font-semibold text-white transition-all"
-            >
-              Save Changes
-            </button>
           </div>
-        </div>
+        )}
+
+        {updateSuccessful && (
+          <div className="flex items-center justify-center h-screen min-h-[500px]">
+            <div className="bg-slate-800 shadow-xl rounded-2xl p-8 w-[420px] text-center border border-slate-700">
+              <div className="flex justify-center mb-4">
+                <div className="w-20 h-20 rounded-full bg-green-500 flex items-center justify-center">
+                  <span className="text-4xl text-white">✓</span>
+                </div>
+              </div>
+
+              <h2 className="text-2xl font-bold text-white mb-2">
+                Course Updated Successfully
+              </h2>
+
+              <p className="text-slate-400 mb-6">
+                Your course details have been updated successfully.
+              </p>
+
+              <div className="flex gap-3">
+                <button
+                  onClick={() => setUpdateSuccessful(false)}
+                  className="flex-1 bg-slate-700 hover:bg-slate-600 transition-all py-3 rounded-lg text-white font-semibold"
+                >
+                  Continue Editing
+                </button>
+
+                <button
+                  onClick={() => navigate("/dashboard/courses")}
+                  className="flex-1 bg-blue-600 hover:bg-blue-700 transition-all py-3 rounded-lg text-white font-semibold"
+                >
+                  View Courses
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     )
   );
