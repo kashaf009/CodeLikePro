abf68e4685e217653be7285d258ad373dc6d1118
kashaf khan <mdkashaf9931@gmail.com>
2026-07-07 18:21:08 +0530

    feat : improved createLeacture section

--- frontend/src/components/dashboard/CreateLecture.jsx
+++ frontend/src/components/dashboard/CreateLecture.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { BASE_URL } from "../../utils/constants";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ImSpinner2 } from "react-icons/im";
 import { useDispatch, useSelector } from "react-redux";
 import { addLecture } from "../../utils/lectureSlice";
+import { IoIosAdd, IoIosArrowRoundBack } from "react-icons/io";
 
 const CreateLecture = () => {
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   const { courseId } = useParams();
   const [lectureTitle, setlectureTitle] = useState("");
@@ -55,13 +57,13 @@ const CreateLecture = () => {
   }, []);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900 px-4">
-      <div className="w-full max-w-xl rounded-2xl bg-slate-800 p-8 shadow-xl">
+    <div className="min-h-screen flex  justify-center bg-slate-900 px-4">
+      <div className="w-full max-w-2xl mb-20 min-h-screen mt-20 rounded-2xl bg-slate-800 px-8 pt-10 shadow-xl">
         <h2 className="mb-6 text-center text-white text-3xl font-bold ">
           Create Lecture
         </h2>
 
-        <div className="mb-6">
+        <div className={error ?`mb-3`:`mb-6`}>
           <label
             htmlFor="lectureTitle"
             className="mb-2 block text-sm font-medium text-gray-200"
@@ -72,24 +74,36 @@ const CreateLecture = () => {
           <input
             id="lectureTitle"
             onChange={(e) => setlectureTitle(e.target.value)}
+            onClick={()=>seterror(null)}
             type="text"
             placeholder="Enter lecture title"
             className="w-full rounded-lg border border-gray-200 text-white px-4 py-3 outline-none placeholder:text-gray-400 transition focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
           />
         </div>
-        {error && <p className="text-red-500 mb-2 mt-2">{error}</p>}
+        {error && <p className="text-red-500 mb-2">{error}</p>}
+
+    <div className="flex gap-4 justify-between">
+      <button
+          onClick={()=>navigate(`/dashboard/create-course/edit/${courseId}`)}
+          
+          className="w-[30%] rounded-lg bg-gray-200 py-3 text-black font-semibold transition hover:bg-gray-400"
+        ><p className="flex items-center justify-center gap-1"><IoIosArrowRoundBack className="text-2xl" />  <p>Back</p></p>
+         
+        </button>
 
         <button
           onClick={HandleAddLecture}
           disabled={loading}
-          className="w-full rounded-lg bg-blue-600 py-3 text-white font-semibold transition hover:bg-blue-700"
-        >
-          {loading ? (
+          className="w-[70%] rounded-lg bg-blue-600 py-3 text-white font-semibold transition hover:bg-blue-700"
+        > 
+          <p className="flex items-center gap-1 justify-center"> <IoIosAdd className="text-2xl" /> {loading ? (
             <ImSpinner2 className="animate-spin mx-auto text-2xl " />
           ) : (
             "Add Lecture"
-          )}
+          )}</p>
         </button>
+    </div>
+        
       </div>
     </div>
   );
